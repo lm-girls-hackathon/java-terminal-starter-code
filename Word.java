1dd285cc22cbaca4b1b6fe188561f7dd16af132d
jonathan-d-zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2022-01-12 11:03:04 -0500

    remove impls to let the competitors do it

--- Word.java
+++ Word.java
@@ -24,32 +24,9 @@ public class Word {
 	 * @return true if the guess was in the word, false otherwise.
 	 */
 	public boolean guessLetter(char guess) {
-		// initialize a boolean to store whether or not the character is in the word
-		boolean found = false;
+		// TODO: Implement this method!
 		
-		char upperCased = Character.toUpperCase(guess);
-		char lowerCased = Character.toLowerCase(guess);
-		
-		// loop over the word
-		for (int i = 0; i < this.answer.length; i++) {
-			// check if the character at the current index is equal to the guess.
-			// we use the uppercased and lowercased version of the guess, so we can find all matches without
-			// worrying if the character is upper or lowercase
-			if (this.answer[i] == upperCased) {
-				
-				// update the display word with the guess
-				this.displayWord[i] = upperCased;
-				
-				// signal that we've found a match
-				found = true;
-			} else if (this.answer[i] == lowerCased) {
-				this.displayWord[i] = lowerCased;
-				
-				found = true;
-			}
-		}
-		
-		return found;
+		return true;
 	}
 	
 	
@@ -59,35 +36,19 @@ public class Word {
 	 * @return true if all the characters match, false otherwise
 	 */
 	public boolean solved() {
-		// loop over the word
-		for (int i = 0; i < this.answer.length; i++) {
-			
-			// if any character doesn't match, return false
-			if (this.answer[i] != this.displayWord[i]) {
-				return false;
-			}
-		}
-		
-		// if none of the characters don't match, that means all the characters do match
-		// which means the word is solved!
+		// TODO: Implement this method!
+
 		return true;
 	}
 	
-	
-	// return the length of the word
 	public int length() {
 		return this.answer.length;
 	}
 	
 	
 	public String toString() {
-		String output = "";
+		// TODO: Implement this method to return a string representation of the Word
 		
-		// Concatenate all chars into a string to display.
-		for (int i = 0; i < this.displayWord.length; i++) {
-			output += displayWord[i];
-		}
-		
-		return output;
+		return super.toString();
 	}
 }
